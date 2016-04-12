477b74dd8898a08bc30c819bf88ab25936ccc519
Christian Nelson <christian.nelson@eit.lth.se>
2016-04-12 09:58:28 +0200

    Fixed the rest of plot geometry.

--- src/V2V_plot_geometry.m
+++ src/V2V_plot_geometry.m
@@ -8,39 +8,24 @@ function V2V_plot_geometry(p)
 % A new figure containing the geometry
 %
 
+tailLength = 50;
+
 xRx0 = p.xRx0(1,:);
 xTx0 = p.xTx0(1,:);
 
 figure; hold on
 
-plot(p.xD(:,1),     p.xD(:,2),   'k.','markersize', 8, 'linewidth', 1.5)
-plot(p.xMD0(:,1),   p.xMD0(:,2), 'b*','markersize', 8, 'linewidth', 1.5)
-plot(p.xSD(:,1),    p.xSD(:,2),  'ms','markersize', 8, 'linewidth', 1.5)
+plot(p.xD(:,1),     p.xD(:,2),   'k.', 'MarkerSize', 8, 'LineWidth', 1.5)
+plot(p.xMD0(:,1),   p.xMD0(:,2), 'b*', 'MarkerSize', 8, 'LineWidth', 1.5)
+plot(p.xSD(:,1),    p.xSD(:,2),  'ms', 'MarkerSize', 8, 'LineWidth', 1.5)
 
-plot(xTx0(1),xTx0(2),'rv','markersize',8,'linewidth',1.5)
-plot(xRx0(1),xRx0(2),'rd','markersize',8,'linewidth',1.5)
+plot(xTx0(1), xTx0(2), 'rv', 'MarkerSize', 8, 'LineWidth', 1.5)
+plot(xRx0(1), xRx0(2), 'rd', 'MarkerSize', 8, 'LineWidth', 1.5)
 
-if p.vTx(1) > 0
-    plot([xTx0(1) xTx0(1)+50], [xTx0(2) xTx0(2)], 'r-', 'linewidth',1.5)
-else
-    plot([xTx0(1) xTx0(1)-50],[xTx0(2) xTx0(2)], 'r-', 'linewidth',1.5)
-end
+plot([xTx0(1) xTx0(1)+sign(p.vTx(1))*tailLength], [xTx0(2) xTx0(2)], 'r-', 'LineWidth', 1.5)
+plot([xRx0(1) xRx0(1)+sign(p.vRx(1))*tailLength], [xRx0(2) xRx0(2)], 'r-', 'LineWidth', 1.5)
 
-if p.vRx(1) > 0
-    L_line = line([xRx0(1) xRx0(1)+50],[xRx0(2) xRx0(2)]); set(L_line,'linestyle','-','color','r','linewidth',1.5)
-else
-    L_line = line([xRx0(1) xRx0(1)-50],[xRx0(2) xRx0(2)]); set(L_line,'linestyle','-','color','r','linewidth',1.5)
-end
-
-
-
-for ctr_MD = 1:p.N_MD
-    if p.vMD(ctr_MD,1) < 0
-        L_line = line([p.xMD0(ctr_MD,1) p.xMD0(ctr_MD,1)-50],[p.xMD0(ctr_MD,2) p.xMD0(ctr_MD,2)]); set(L_line,'linestyle','-','color','b','linewidth',1.5)
-    else
-        L_line = line([p.xMD0(ctr_MD,1) p.xMD0(ctr_MD,1)+50],[p.xMD0(ctr_MD,2) p.xMD0(ctr_MD,2)]); set(L_line,'linestyle','-','color','b','linewidth',1.5)
-    end
-end
+plot([p.xMD0(:,1) p.xMD0(:,1)+sign(p.vMD(:,1))*tailLength]',[p.xMD0(:,2) p.xMD0(:,2)]', 'b-', 'LineWidth', 1.5)
 
 set(gca, ...
     'FontSize', 14, ...
